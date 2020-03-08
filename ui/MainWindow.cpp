b73ac6081b794b2d4a0691e3dc52bd87682c7b01
shibafu <shibafu528@gmail.com>
2020-03-09 08:44:35 +0900

    add 虚無, 誰か助けてくれ

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -90,6 +90,68 @@ static bool parse_message(grpc::ByteBuffer &buffer, google::protobuf::Message &m
     return message.ParseFromString(buf);
 }
 
+static QString error_code_to_string(grpc::StatusCode statusCode) {
+    QString code;
+    switch (statusCode) {
+        case grpc::OK:
+            code = "OK";
+            break;
+        case grpc::CANCELLED:
+            code = "CANCELLED";
+            break;
+        case grpc::UNKNOWN:
+            code = "UNKNOWN";
+            break;
+        case grpc::INVALID_ARGUMENT:
+            code = "INVALID_ARGUMENT";
+            break;
+        case grpc::DEADLINE_EXCEEDED:
+            code = "DEADLINE_EXCEEDED";
+            break;
+        case grpc::NOT_FOUND:
+            code = "NOT_FOUND";
+            break;
+        case grpc::ALREADY_EXISTS:
+            code = "ALREADY_EXISTS";
+            break;
+        case grpc::PERMISSION_DENIED:
+            code = "PERMISSION_DENIED";
+            break;
+        case grpc::UNAUTHENTICATED:
+            code = "UNAUTHENTICATED";
+            break;
+        case grpc::RESOURCE_EXHAUSTED:
+            code = "RESOURCE_EXHAUSTED";
+            break;
+        case grpc::FAILED_PRECONDITION:
+            code = "FAILED_PRECONDITION";
+            break;
+        case grpc::ABORTED:
+            code = "ABORTED";
+            break;
+        case grpc::OUT_OF_RANGE:
+            code = "OUT_OF_RANGE";
+            break;
+        case grpc::UNIMPLEMENTED:
+            code = "UNIMPLEMENTED";
+            break;
+        case grpc::INTERNAL:
+            code = "INTERNAL";
+            break;
+        case grpc::UNAVAILABLE:
+            code = "UNAVAILABLE";
+            break;
+        case grpc::DATA_LOSS:
+            code = "DATA_LOSS";
+            break;
+        default:
+            code = "?";
+            break;
+    }
+    code += QString::asprintf(" (%d)", statusCode);
+    return code;
+}
+
 void MainWindow::onExecuteButtonClicked() {
     if (currentMethod == nullptr) {
         return;
@@ -143,7 +205,7 @@ void MainWindow::onExecuteButtonClicked() {
             QString result;
             QTextStream stream(&result);
 
-            stream << "[gRPC Error]\n - Error Code: " << status.error_code() <<
+            stream << "[gRPC Error]\n - Error Code: " << error_code_to_string(status.error_code()) <<
                 "\n - Error Message: " << QString::fromStdString(status.error_message()) <<
                 "\n - Details:\n" << QString::fromStdString(status.error_details());
             ui.responseEdit->setText(result);
