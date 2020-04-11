55b56e1e6a426531d5bf59d68b2fb7fa659caaeb
shibafu <shibafu528@gmail.com>
2020-04-12 01:36:41 +0900

    CryptoAPIでWindowsにインストールされた証明書を読みこみ、GRPC Coreから参照できるようにした

--- main.cpp
+++ main.cpp
@@ -7,12 +7,52 @@
 #include <QFont>
 #include <QFontDatabase>
 #include <QLocale>
+#include <grpc/grpc_security.h>
+#include <wincrypt.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>
 #endif
 
 #ifdef __APPLE__
 #include <QOperatingSystemVersion>
 #include <QFont>
 #include <QLocale>
+#endif
+
+#ifdef _WIN32
+grpc_ssl_roots_override_result callbackSSLRootsOverrideWin32(char** pem_root_certs) {
+    auto hStore = CertOpenSystemStore(NULL, "ROOT");
+    if (hStore == nullptr) {
+        return GRPC_SSL_ROOTS_OVERRIDE_FAIL;
+    }
+
+    BIO *pemBuffer = BIO_new(BIO_s_mem());
+    PCCERT_CONTEXT pCertContext = nullptr;
+    while (pCertContext = CertEnumCertificatesInStore(hStore, pCertContext)) {
+        const unsigned char* der = pCertContext->pbCertEncoded;
+        X509* x509 = d2i_X509(nullptr, &der, pCertContext->cbCertEncoded);
+        if (x509 != nullptr) {
+            PEM_write_bio_X509(pemBuffer, x509);
+            X509_free(x509);
+        }
+    }
+    CertCloseStore(hStore, 0);
+
+    void *pemData;
+    long pemLength = BIO_get_mem_data(pemBuffer, &pemData);
+    char *pemDup = static_cast<char*>(malloc(pemLength + 1));
+    if (pemDup == nullptr) {
+        BIO_free(pemBuffer);
+        return GRPC_SSL_ROOTS_OVERRIDE_FAIL;
+    }
+    pemDup[pemLength] = 0;
+    memcpy(pemDup, pemData, pemLength);
+    BIO_free(pemBuffer);
+
+    *pem_root_certs = pemDup;
+
+    return GRPC_SSL_ROOTS_OVERRIDE_OK;
+}
 #endif
 
 int main(int argc, char *argv[]) {
@@ -30,6 +70,8 @@ int main(int argc, char *argv[]) {
             app.setFont(fontMeiryo);
         }
     }
+
+    grpc_set_ssl_roots_override_callback(callbackSSLRootsOverrideWin32);
 #endif
 
 #ifdef __APPLE__
